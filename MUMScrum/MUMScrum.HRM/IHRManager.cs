d3490be1369ce11ee097704eca0718e26e0d8d54
SM Moniruzaman <Rasel@SMs-MacBook-Pro.local>
2016-02-01 17:08:11 -0600

    Additng HR classes

---
+++ MUMScrum/MUMScrum.HRM/IHRManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MUMScrum.Entity;
+
+namespace MUMScrum.HRM
+{
+    public interface IHRManager
+    {
+        Employee Login(string username, string password);
+        CreateEmployeeStatus CreateEmployee(Employee employee);
+        CreateEmployeeStatus UpdateEmployee(Employee employee);
+        CreateEmployeeStatus UpdateProfile(Employee employee);
+        void AssignRole(int empId, int roleId);
+        void DeleteEmployee(int empId);
+        IEnumerable<Employee> GetAllEmployees();
+        IEnumerable<Employee> GetAllDevelopers();
+        IEnumerable<Employee> GetAllTesters();
+        Employee GetEmployeeById(int id);
+        IEnumerable<Role> GetAllRoles();
+
+        IEnumerable<Employee> GetScrumMaster();
+
+    }
+}
